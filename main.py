65d0cb3c5ef69b9f27638b4c5462df1c22c6ccb3
Noah <554456@student.saxion.nl>
2023-11-20 21:33:00 +0100

    Made changes to the calculator

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ third_row_calculator = [1, 2, 3, "+"]
 fourth_row_calculator = [0, ",", "="]
 
 def calculator_board():
+
     print("""
-        7 | 8 | 9 | X
-        4 | 5 | 6 | -
-        1 | 2 | 3 | +
-        0 | , | =
+       -----------------
+       | 7 | 8 | 9 | X |
+       | 4 | 5 | 6 | - |
+       | 1 | 2 | 3 | + |
+       | 0 | , | = | R |
+       -----------------
         """)
 
 def main():
     print("*** Welcome to the calculator! ***")
+    print("Here you are free to calculate everything you want to!")
+    print("The R stands for reset!")
 
     calculator_board()
     
